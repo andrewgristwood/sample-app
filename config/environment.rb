2ef46afadbb8ae5d05819e3d16e151e15c4b071e
Andrew Gristwood <username@users.noreply.github.com>
2017-06-08 17:37:35 +0100

    Add static pages

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# Load the Rails application.
+# Load the Rails application.html.erb.
 require_relative 'application'
 
-# Initialize the Rails application.
+# Initialize the Rails application.html.erb.
 Rails.application.initialize!
--- config/puma.rb
+++ config/puma.rb
@@ -17,15 +17,15 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
+# the concurrency of the application.html.erb would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
+# This directive tells Puma to first boot the application.html.erb and load code
+# before forking the application.html.erb. This takes advantage of Copy On Write
 # process behavior so workers use less memory. If you use this option
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
@@ -36,7 +36,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # clustered mode by specifying a number of `workers`. After each worker
 # process is booted this block will be run, if you are using `preload_app!`
 # option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
+# or connections that may have been created at application.html.erb boot, Ruby
 # cannot share connections between processes.
 #
 # on_worker_boot do
